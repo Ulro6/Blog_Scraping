a64bbec57d393223ad023ca0e690af0706e5972e
Ulro6 <quentin.wasser@online.fr>
2024-03-22 17:08:51 +0100

    Est ce que ca corrige en ligne

--- MonSite/blog/urls.py
+++ MonSite/blog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 from django.urls import re_path
+from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf import settings    
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     re_path(r'^media/(?P<path>.*)$', serve,
             {'document_root': settings.MEDIA_ROOT}),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
