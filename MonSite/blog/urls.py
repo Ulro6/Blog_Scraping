7db8c101dc9a489c766c7c0a0c18bcd011795909
Ulro6 <quentin.wasser@online.fr>
2024-03-22 17:12:24 +0100

    Encore un changement dans url avec debug

--- MonSite/blog/urls.py
+++ MonSite/blog/urls.py
@@ -6,9 +6,13 @@ from django.views.static import serve
 from django.conf import settings    
 
 
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     re_path(r'^media/(?P<path>.*)$', serve,
             {'document_root': settings.MEDIA_ROOT}),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
