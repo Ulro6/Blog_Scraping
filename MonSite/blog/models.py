ba9647587dc04ef314a2edbe6f71f82d5f8b8cdf
Ulro6 <quentin.wasser@online.fr>
2024-03-25 18:51:19 +0100

    Sauter lignes

--- MonSite/blog/models.py
+++ MonSite/blog/models.py
@@ -4,8 +4,6 @@ from django.db import models
 from django.utils import timezone
 
 
-
-
     
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
